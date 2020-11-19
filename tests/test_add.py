04b51f19e47e400e39fe39bf573f7fb7e03071b2
Saptarshi <saptarshi14mitra@gmail.com>
2020-11-19 22:34:21 +0100

    working test for an add operation. in ConfirmedStudents script the test add function is commented

--- tests/test_add.py
+++ tests/test_add.py
@@ -4,3 +4,14 @@
 # @Email   : saptarshi.mitra@tum.de
 # @File    : test_add.py
 # @Project: eda-seminar-organization-grading
+
+from toolscripts.ConfirmedStudents import add
+
+print("before test")
+print(add(15,16))
+
+#function for testing in pytest
+def test_add():
+    res = add(1, 1)
+    assert res == 2
+    print("after test")
