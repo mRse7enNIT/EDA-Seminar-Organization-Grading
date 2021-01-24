b04bae18ff520ff25772b2d20feb9d021b1e191e
saptarshi <saptarshi14mitra@gmail.com>
2021-01-24 22:24:21 +0100

    Commented and cleaned Script2

--- toolscripts/GraderSheets.py
+++ toolscripts/GraderSheets.py
@@ -16,7 +16,7 @@ import argparse
 import os
 
 def create_arg_parser():
-    # Creates and returns the ArgumentParser object
+    """Creates and returns the ArgumentParser object"""
 
     parser = argparse.ArgumentParser(description='Gives confirmed list of students with review allocation')
     parser.add_argument('inputFile',
@@ -28,11 +28,7 @@ def create_arg_parser():
     return parser
 
 def read_srcfile(source_filename):
-    # Import the src file from TUMonline into a Pandas dataframe
-    #src_xlsx = pd.ExcelFile(source_filename)
-    #print("Sheets present in the src xlsx file: \n")
-    #print(src_xlsx.sheet_names)
-    #src_df = src_xlsx.parse('TUMonline_2020-11-07')
+    """Import the src file from TUMonline into a Pandas dataframe"""
     src_wb = load_workbook(source_filename)
     print("The available sheets in the xlsx file")
     print(src_wb.sheetnames)
@@ -43,44 +39,26 @@ def read_srcfile(source_filename):
     return src_df
 
 
-def write_masterfile(write_df):
-    # master_wb = Workbook()
-    # current_ws = master_wb.active
-    # for r in dataframe_to_rows(write_df, index=True, header= True):
-    #     current_ws.append(r)
-    # for cell in current_ws['A'] + current_ws[1]:
-    #     cell.style = 'Pandas'
-    #
-    # master_wb.save("OutputFiles/master_sheet_" + str(pd.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + ".xlsx")
-    writer = pd.ExcelWriter("OutputFiles/master_sheet_" + str(pd.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')) + ".xlsx", engine='xlsxwriter')
-    write_df.to_excel(writer,'Sheet1')
-    writer.save()
 
 
 def replace_header(input_df):
+    """replace headers of the dataframe with first row of sheet"""
     new_header = input_df.iloc[0]
     input_df = input_df[1:]
     input_df.columns=new_header
     return input_df
 
 
-def choose_fixed_place(modified_df):
-    # for (idx,row) in modified_df.iterrows():
-    #     print(idx,row)
-    filtered_df = modified_df[(modified_df.STATUS=='Fixplatz')]
-    filtered_df.reset_index(inplace=True, drop=True)
-    filtered_df = filtered_df[['FAMILIENNAME','VORNAME','MATRIKELNUMMER','GESCHLECHT','E-MAIL']]
-    return filtered_df
 
 
 def generate_supervisor_files(modified_df):
+    """Create an individual file with supervisor name, then fill it which required pages from template with customized cells, filled by data from MasterSheets"""
     supervisor_list = modified_df.BETREUER.unique()
     print("Here are the list of supervisors")
     print(supervisor_list)
-    for supervisor in supervisor_list:
+    for supervisor in supervisor_list:      #iterate through all unique supervisors and add pages
         print(supervisor)
         writer = pd.ExcelWriter("OutputFiles/GraderSheets/" + supervisor + "_GradingSheetSeminar" + ".xlsx",engine='xlsxwriter')
-        #blank_dataframe = pd.DataFrame()
         overview_dataframe = add_overview_sheet(supervisor)
         overview_dataframe.to_excel(writer, 'Overview')
         paper_grading_dataframe = add_paper_grading_sheet(supervisor, modified_df)
@@ -93,6 +71,7 @@ def generate_supervisor_files(modified_df):
 
 
 def add_overview_sheet(supervisor):
+    """Adding the overview sheet with generic instructions and name of the supervisor"""
     src_grading_wb = load_workbook('DataSources/Foik_GradingSheetSeminar.xlsx')
     print("The available sheets in the xlsx file")
     print(src_grading_wb.sheetnames)
@@ -105,6 +84,7 @@ def add_overview_sheet(supervisor):
 
 
 def add_paper_grading_sheet(supervisor, modified_df):
+    """Adding the Paper Grading sheet with all supervised student Papers and grading metrics"""
     src_grading_wb = load_workbook('DataSources/Foik_GradingSheetSeminar.xlsx')
     print("The available sheets in the xlsx file")
     print(src_grading_wb.sheetnames)
@@ -136,6 +116,7 @@ def add_paper_grading_sheet(supervisor, modified_df):
 
 
 def add_review_grading_sheet(supervisor, modified_df):
+    """Adding the Review Grading sheet with all students who reviewed the Papers authored by the students who were supervised by the him/her and grading metrics"""
     src_grading_wb = load_workbook('DataSources/Foik_GradingSheetSeminar.xlsx')
     print("The available sheets in the xlsx file")
     print(src_grading_wb.sheetnames)
@@ -170,18 +151,16 @@ def add_review_grading_sheet(supervisor, modified_df):
 
 
 def add_presentations_sheet(supervisor, modified_df):
+    """Adding the Presentations sheet with all Presentations according to the talk order on the presentation day"""
     src_grading_wb = load_workbook('DataSources/Foik_GradingSheetSeminar.xlsx')
     print("The available sheets in the xlsx file")
     print(src_grading_wb.sheetnames)
     src_sheet = src_grading_wb["Presentations"]
     print("selected sheet for data manipulation:")
     print(src_sheet)
-    #src_sheet['E9'] = supervisor
     review_grading_df = pd.DataFrame(src_sheet.values)
     reviewer_interaction_df = review_grading_df[3:13]
     review_grading_df = review_grading_df[:1]     # taking only first rows from template
-    # supervision_df = modified_df[(modified_df.BETREUER == supervisor)]
-    # supervision_df = supervision_df[['FAMILIENNAME', 'VORNAME', 'MATRIKELNUMMER', 'TITEL', 'REVIEW_VON']]
 
     for talk_number in range(len(modified_df)):
         for indextalk, rowtalk in modified_df.iterrows():
@@ -199,53 +178,19 @@ def add_presentations_sheet(supervisor, modified_df):
         review_grading_df.loc[len(review_grading_df)] = ""
         review_grading_df.loc[len(review_grading_df)] = ""
 
-
     return review_grading_df
 
 
 
 
 
-def add_columns(filtered_df):
-    print("adding additional columns for TITEL, BETREUER, VORTRAG")
-    extended_df = filtered_df.assign(TITEL='', BETREUER='', VORTRAG='')
-    return extended_df
 
-
-def add_columns_HS(extended_df):
-    print("adding additional columns for REVIEW FÜR (AKTIVE), REVIEW VON (PASSIV)")
-    extended_df = extended_df.assign(REVIEW_FÜR='', REVIEW_VON='')
-    return extended_df
-
-
-def shuffle_review(extended_df):
-    print("Shuffling the review for column to assign a student report to another student")
-    #uncomment this section if you want random shuffling
-    # shuffled_review_column = extended_df.drop(extended_df.columns.difference(['REVIEW_FÜR']), 1)
-    # shuffled_review_column['REVIEW_FÜR'] = extended_df['MATRIKELNUMMER']
-    # shuffled_review_column = shuffled_review_column.sample(frac=1)
-    # shuffled_review_column.reset_index(inplace=True, drop=True)
-    # shuffled_df = extended_df
-    # shuffled_df['REVIEW_FÜR'] = shuffled_review_column['REVIEW_FÜR']
-    # shuffled_df_copy = shuffled_df
-
-    #shift by 1 review allotement
-    for index, row in extended_df.iterrows():
-        if index < (extended_df.shape[0]- 1):
-            row['REVIEW_FÜR'] = extended_df.iloc[index+1]['MATRIKELNUMMER']
-        else:
-            row['REVIEW_FÜR'] = extended_df.iloc[0]['MATRIKELNUMMER']
-    for index, row in extended_df.iterrows():
-        if index != 0:
-            row['REVIEW_VON'] = extended_df.iloc[index-1]['MATRIKELNUMMER']
-        else:
-            row['REVIEW_VON'] = extended_df.iloc[extended_df.shape[0]-1]['MATRIKELNUMMER']
-    return extended_df
 
 
 def main():
+    """Entry point for Scipt2 (Preparing the GradingSheets for each supervisors from MasterSheet produced by Script1"""
     arg_parser = create_arg_parser()
-    parsed_args = arg_parser.parse_args(sys.argv[1:])
+    parsed_args = arg_parser.parse_args(sys.argv[1:])        #parsing arguments after first one(self)
     if os.path.exists(parsed_args.inputFile):
         print("Input File exists")
         #src_df = read_srcfile(parsed_args.inputFile)
@@ -253,11 +198,10 @@ def main():
         print(src_df.to_string())
         modified_df = replace_header(src_df)
         print(modified_df)
-        modified_df = modified_df.loc[:, modified_df.columns.notnull()]
+        modified_df = modified_df.loc[:, modified_df.columns.notnull()]     #shaping the dataframe for sheets
         modified_df.reset_index(inplace=True, drop=True)
-        # filtered_df = choose_fixed_place(modified_df)
-        # print(filtered_df)
-        # extended_df = add_columns(filtered_df)
+
+    generate_supervisor_files(modified_df)
 
     # if(parsed_args.updatepath):
     #     print("Updating student information in {}".format(parsed_args.updatepath))
@@ -285,7 +229,7 @@ def main():
     #     extended_df = add_columns_HS(extended_df)
     #     extended_df = shuffle_review(extended_df)
 
-    generate_supervisor_files(modified_df)
+
     #write_masterfile(extended_df)
 
 if __name__ == "__main__":
